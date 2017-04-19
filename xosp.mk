f6f87200996c7362f434be65d9c36e6c549ae501
ElDainosor <eldainosor@gmail.com>
2017-04-19 22:13:00 +0000

    XOSP new bringup
    Rebasing everything and now this will work

--- xosp.mk
+++ xosp.mk
@@ -21,10 +21,12 @@ TARGET_SCREEN_HEIGHT := 960
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := surnia
-PRODUCT_NAME := lineage_surnia
+PRODUCT_NAME := xosp_surnia
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := surnia
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="Moto E LTE (2nd gen)"
+    
+RELEASE_TYPE := XOSP_OFFICIAL
